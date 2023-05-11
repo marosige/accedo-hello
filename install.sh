ddba673479d13e1f4f76ea4003d0c51d3777aa08
Marosi Gergely <gergely.marosi@accedo.tv>
2023-05-11 14:17:39 +0200

    removed false br lines from script

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-<br> brew cask install google-chrome
+brew cask install google-chrome
 
-# <br> brew cask install slack
+# brew cask install slack
 
-# <br> brew cask install 1password
+# brew cask install 1password
 
-# <br> brew cask install android-studio
+# brew cask install android-studio
 
-<br> brew cask install atom
+brew cask install atom
 
-<br> brew install neofetch
+brew install neofetch
 
-# <br> brew cask install spotify
+# brew cask install spotify
 
-<br> brew cask install tiles
+brew cask install tiles
